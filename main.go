06e06bd152dbe6e91f7ce91c16dcd566be2e81e4
Brandur <brandur@mutelight.org>
2016-01-05 12:11:16 -0700

    Have work operate in parallel

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/russross/blackfriday"
@@ -21,11 +22,19 @@ const (
 	TargetDir   = "./public/"
 )
 
+var (
+	verbose = false
+)
+
 func main() {
 	start := time.Now()
 	defer func() {
 		fmt.Printf("Site built in %v\n", time.Now().Sub(start))
 	}()
+
+	if os.Getenv("VERBOSE") == "true" {
+		verbose = true
+	}
 
 	// create an output directory
 	err := os.MkdirAll(TargetDir, 0755)
@@ -33,34 +42,109 @@ func main() {
 		panic(err)
 	}
 
-	err = linkAssets()
+	articleJobs, err := generateArticleJobs()
 	if err != nil {
 		panic(err)
 	}
 
-	err = renderArticles()
+	pageJobs, err := generatePageJobs()
 	if err != nil {
 		panic(err)
 	}
 
-	err = renderPages()
-	if err != nil {
-		panic(err)
+	// we build jobs for everything and then just run it all in parallel
+	var jobs []func() error
+	jobs = append(jobs, func() error {
+		return linkAssets()
+	})
+	jobs = append(jobs, articleJobs...)
+	jobs = append(jobs, pageJobs...)
+
+	errors := make([]error, len(jobs))
+
+	var wg sync.WaitGroup
+	for i, job := range jobs {
+		wg.Add(1)
+
+		// be careful with closures in loops
+		localJob := job
+
+		go func() {
+			defer wg.Done()
+
+			// avoiding an append() keeps this safe between goroutines
+			errors[i] = localJob()
+		}()
+	}
+	wg.Wait()
+
+	// should probably have a more complete approach to error handling here
+	for _, err := range errors {
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
+func generateArticleJobs() ([]func() error, error) {
+	files, err := ioutil.ReadDir(ArticlesDir)
+	if err != nil {
+		return nil, err
+	}
+
+	jobs := make([]func() error, len(files))
+
+	for i, fileInfo := range files {
+		// be careful with closures in loops
+		localFileInfo := fileInfo
+
+		jobs[i] = func() error {
+			return renderArticle(localFileInfo.Name())
+		}
+	}
+
+	return jobs, nil
+}
+
+func generatePageJobs() ([]func() error, error) {
+	files, err := ioutil.ReadDir(PagesDir)
+	if err != nil {
+		return nil, err
+	}
+
+	jobs := make([]func() error, len(files))
+
+	for i, fileInfo := range files {
+		// be careful with closures in loops
+		localFileInfo := fileInfo
+
+		jobs[i] = func() error {
+			// ace.Load doesn't like to take .ace extensions anyway
+			return renderPage(trimExtension(localFileInfo.Name()))
+		}
+	}
+
+	return jobs, nil
+}
+
 func linkAssets() error {
-	err := os.RemoveAll(TargetDir + "assets")
+	if verbose {
+		fmt.Printf("Linking assets directory\n")
+	}
+
+	err := os.RemoveAll(TargetDir + AssetsDir)
 	if err != nil {
 		return err
 	}
 
-	source, err := filepath.Abs("./assets")
+	// we use absolute paths for source and destination because not doing so
+	// can result in some weird symbolic link inception
+	source, err := filepath.Abs(AssetsDir)
 	if err != nil {
 		return err
 	}
 
-	dest, err := filepath.Abs(TargetDir + "assets")
+	dest, err := filepath.Abs(TargetDir + AssetsDir)
 	if err != nil {
 		return err
 	}
@@ -73,39 +157,34 @@ func linkAssets() error {
 	return nil
 }
 
-func renderArticles() error {
-	files, err := ioutil.ReadDir(ArticlesDir)
+func renderArticle(articleFile string) error {
+	if verbose {
+		fmt.Printf("Rendered article '%v'\n", articleFile)
+	}
+
+	source, err := ioutil.ReadFile(ArticlesDir + articleFile)
+	if err != nil {
+		return err
+	}
+	rendered := renderMarkdown(source)
+
+	template, err := ace.Load(LayoutsDir+"main", LayoutsDir+"article", nil)
 	if err != nil {
 		return err
 	}
 
-	for _, fileInfo := range files {
-		articleFile := fileInfo.Name()
+	file, err := os.Create(TargetDir + trimExtension(articleFile))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-		source, err := ioutil.ReadFile(ArticlesDir + articleFile)
-		if err != nil {
-			return err
-		}
-		rendered := renderMarkdown(source)
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
 
-		template, err := ace.Load(LayoutsDir+"main", LayoutsDir+"article", nil)
-		if err != nil {
-			return err
-		}
-
-		file, err := os.Create(TargetDir + trimExtension(articleFile))
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		writer := bufio.NewWriter(file)
-		defer writer.Flush()
-
-		err = template.Execute(writer, map[string]string{"Content": string(rendered)})
-		if err != nil {
-			return err
-		}
+	err = template.Execute(writer, map[string]string{"Content": string(rendered)})
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -134,34 +213,28 @@ func renderMarkdown(source []byte) []byte {
 	return blackfriday.Markdown(source, renderer, extensions)
 }
 
-func renderPages() error {
-	files, err := ioutil.ReadDir(PagesDir)
+func renderPage(pageFile string) error {
+	if verbose {
+		fmt.Printf("Rendered page '%v'\n", pageFile)
+	}
+
+	template, err := ace.Load(LayoutsDir+"main", PagesDir+pageFile, nil)
 	if err != nil {
 		return err
 	}
 
-	for _, fileInfo := range files {
-		// ace.Load doesn't like to take .ace extensions anyway
-		pageFile := trimExtension(fileInfo.Name())
+	file, err := os.Create(TargetDir + pageFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-		template, err := ace.Load(LayoutsDir+"main", PagesDir+pageFile, nil)
-		if err != nil {
-			return err
-		}
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
 
-		file, err := os.Create(TargetDir + pageFile)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		writer := bufio.NewWriter(file)
-		defer writer.Flush()
-
-		err = template.Execute(writer, map[string]string{})
-		if err != nil {
-			return err
-		}
+	err = template.Execute(writer, map[string]string{})
+	if err != nil {
+		return err
 	}
 
 	return nil
