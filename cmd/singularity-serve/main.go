2822cb6cbe3a9153757b045a1976941ae3518e64
Brandur <brandur@mutelight.org>
2016-03-27 18:23:44 -0700

    Remove unneeded newline

--- cmd/singularity-serve/main.go
+++ cmd/singularity-serve/main.go
@@ -37,7 +37,7 @@ func main() {
 }
 
 func serve(port int) error {
-	log.Infof("Serving '%v' on port %v\n", path.Clean(singularity.TargetDir), port)
+	log.Infof("Serving '%v' on port %v", path.Clean(singularity.TargetDir), port)
 	handler := http.FileServer(http.Dir(singularity.TargetDir))
 	return http.ListenAndServe(":"+strconv.Itoa(port), handler)
 }
